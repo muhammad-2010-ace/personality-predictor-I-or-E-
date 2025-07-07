e748e4c9c9cfc147e9a644563c76b89496f1a924
muhammad-2010-ace <taoheedmyname2@gmail.com>
2025-07-07 05:03:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,16 @@ import numpy as np
 with open("personality_model.pkl","rb") as file:
   model=pickle.load(file)
 
-st.title("Personality Predictor(Introvert vs Extrovert)")
+st.title("Personality Predictor(Introvert vs Extrovert) By Muhammad Taoheed")
 st.markdown("### Please enter the following info:")
 
-time_alone = st.slider("Time Spent Alone(0-10)",0.0,10.0,5.0)
+time_alone = st.slider("Time Spent Alone(rate:0-10)",0.0,10.0,5.0)
 stage_fear = st.radio("Do you have stage fear?",["Yes","No"])
-social_event = st.slider("Social Event Attendance(0-10)",0.0,10.0,5.0)
-going_outside = st.slider("How often do you go outside?(0-10)",0.0,10.0,5.0)
+social_event = st.slider("Social Event Attendance(rate:0-10)",0.0,10.0,5.0)
+going_outside = st.slider("How often do you go outside?(rate:0-10)",0.0,10.0,5.0)
 drained_socializing = st.radio("Do you feel drained after socializing?",["Yes","No"])
-friend_circle = st.slider("Size of friends circle(0-10)",0.0,10.0,5.0)
-post_frequency = st.slider("How often do you post on social media?(0-10)",0.0,10.0,5.0)
+friend_circle = st.slider("Size of friends circle(rate:0-10)",0.0,10.0,5.0)
+post_frequency = st.slider("How often do you post on social media?(rate:0-10)",0.0,10.0,5.0)
 
 stage_fear = 1 if stage_fear == "Yes" else 0
 drained_socializing = 1 if drained_socializing == "Yes" else 0
